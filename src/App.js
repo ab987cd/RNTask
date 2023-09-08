b7243a92a763450cae28d79a9cefd774e2782b46
Shruti Gupta <shruti.gupta2k12@gmail.com>
2023-09-08 13:02:38 +0530

    Initial Project Setup

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.textStyle}>React Native Task</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textStyle: {
+    fontSize: 18,
+    color:'#000'
+  }
+});
+
+export default App;
